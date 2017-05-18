9429bbad3acac27bc5da0720bd8286a15bd31074
Carl Edquist <edquist@cs.wisc.edu>
2017-05-18 11:22:23 -0500

    make docker-run script slightly noisier

--- opensciencegrid/gracc-apel/docker-run.sh
+++ opensciencegrid/gracc-apel/docker-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
 
+echo Generating APEL report...
 wrote=$(/usr/libexec/apel/apel_report.py)
+echo "$wrote"
 apelfile=${wrote#wrote: }
 
 cp "$apelfile" /var/spool/apel/outgoing/12345678/1234567890abcd
