8c92ba09b181f49817c56b33a6ccb39ca5ad615e
Brian Lin <brianhlin@gmail.com>
2019-03-04 16:45:52 -0600

    Add Travis CI build and deployment to DockerHub

---
+++ opensciencegrid/frontier-squid/travis/build_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -xe
+# Script for building and pushing Frontier Squid docker images
+
+org='opensciencegrid'
+timestamp=`date +%Y%m%d-%H%M`
+docker_repos='frontier-squid'
+
+for repo in $docker_repos; do
+    docker build \
+           -t $org/$repo:development \
+           -t $org/$repo:$timestamp \
+           $repo
+done
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    echo "DockerHub deployment not performed for pull requests"
+    exit 0
+fi
+
+# Credentials for docker push
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+for repo in $docker_repos; do
+    for tag in $timestamp development; do
+        docker push $org/$repo:$tag
+    done
+done
