86a9b72d14b20fe7246756daafab82d683425d0f
David Manglano <david.manglano@gmail.com>
2019-06-20 15:09:35 -0500

    Missing entrypoint causes REST endpoint to fail

--- contrib/fts/docker-entrypoint.sh
+++ contrib/fts/docker-entrypoint.sh
@@ -9,6 +9,8 @@ chown root:root /etc/grid-security/hostkey.pem
 cp /tmp/fts3-configs/fts3config /etc/fts3/fts3config
 cp /tmp/fts3-configs/fts-msg-monitoring.conf /etc/fts3/fts-msg-monitoring.conf
 mkdir -p /var/lib/mysql/
+mkdir -p /var/log/fts3rest/ && touch /var/log/fts3rest/fts3rest.log
+chown fts3:fts3 /var/log/
 touch /var/lib/mysql/mysql.sock
 if [[ ! -z "${DATABASE_UPGRADE}" ]]; then
    yes Y | python /usr/share/fts/fts-database-upgrade.py
