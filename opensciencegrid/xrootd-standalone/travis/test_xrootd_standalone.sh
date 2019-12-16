526bd6114ee8d7b9c823b032267a19488af953e7
efajardo <efajardo@physics.ucsd.edu>
2019-12-16 15:48:08 -0800

    Changing configs around

--- opensciencegrid/xrootd-standalone/travis/test_xrootd_standalone.sh
+++ opensciencegrid/xrootd-standalone/travis/test_xrootd_standalone.sh
@@ -4,10 +4,11 @@
 docker run --rm \
        --publish 1094:1094 \
        --volume $(pwd)/travis/test_file:/tmp/docker_xrootd_standalone/test_file \
-       --volume $(pwd)/travis/15-travis-ci-config.cfg:/etc/xrootd/config.d/15-travis-ci-config.cfg \
-       --name xrootd_standalone opensciencegrid/xrootd-standalone:fresh &
+       --volume $(pwd)/travis/15-travis-site-config.cfg:/etc/xrootd/config.d/15-travis-site-config.cfg \
+       --volume $(pwd)/travis/40-travis-ci-lcmaps.cfg:/etc/xrootd/config.d/90-travis-ci-lcmaps.cfg \
+       --name xrootd_standalone opensciencegrid/xrootd-standalone:20191216-2313 &
 docker ps
-sleep 10
+sleep 2
 
 #online_md5="$(curl -sL http://localhost:1094//test_file | md5sum | cut -d ' ' -f 1)"
 #local_md5="$(md5sum $(pwd)/travis/test_file | cut -d ' ' -f 1)"
