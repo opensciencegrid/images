26633ee2bfab7308105ef319a1975b7a7c29c68c
efajardo <efajardo@physics.ucsd.edu>
2020-01-14 16:38:57 -0800

    Checking for file existense

--- opensciencegrid/xrootd-standalone/image-config.d/10-add-users.sh
+++ opensciencegrid/xrootd-standalone/image-config.d/10-add-users.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-grep '".*" .*' /usr/share/osg/voms-mapfile-default  | awk '{print $2}' | xargs -n1 adduser
-grep '".*" .*' /etc/grid-security/grid-mapfile  | awk '{print $2}' | xargs -n1 adduser
-grep '".*" .*' /etc/grid-security/voms-mapfile | awk '{print $2}' | xargs -n1 adduser
+if [ -f '/usr/share/osg/voms-mapfile-default' ]; then
+    grep '".*" .*' /usr/share/osg/voms-mapfile-default  | awk '{print $2}' | xargs -n1 adduser
+fi
+if [ -f '/etc/grid-security/grid-mapfile' ]; then
+    grep '".*" .*' /etc/grid-security/grid-mapfile  | awk '{print $2}' | xargs -n1 adduser
+fi
+if [ -f '/etc/grid-security/voms-mapfile' ]; then
+    grep '".*" .*' /etc/grid-security/voms-mapfile | awk '{print $2}' | xargs -n1 adduser
+fi
