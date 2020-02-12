ae7289ddf23a69cd7734807a5947aefd3bf25f4e
Edgar Fajardo <efajardo@users.noreply.github.com>
2020-02-12 13:00:03 -0800

    Bug on adding the users automatically

--- opensciencegrid/xrootd-standalone/image-config.d/10-add-users.sh
+++ opensciencegrid/xrootd-standalone/image-config.d/10-add-users.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -f '/usr/share/osg/voms-mapfile-default' ]; then
-    grep '".*" .*' /usr/share/osg/voms-mapfile-default  | awk '{print $2}' | xargs -n1 adduser
+    grep '".*" .*' /usr/share/osg/voms-mapfile-default  | awk '{print $NF}' | xargs -n1 adduser
 fi
 if [ -f '/etc/grid-security/grid-mapfile' ]; then
-    grep '".*" .*' /etc/grid-security/grid-mapfile  | awk '{print $2}' | xargs -n1 adduser
+    grep '".*" .*' /etc/grid-security/grid-mapfile | awk '{print $NF}' | xargs -n1 adduser
 fi
 if [ -f '/etc/grid-security/voms-mapfile' ]; then
-    grep '".*" .*' /etc/grid-security/voms-mapfile | awk '{print $2}' | xargs -n1 adduser
+    grep '".*" .*' /etc/grid-security/voms-mapfile | awk '{print $NF}' | xargs -n1 adduser
 fi
