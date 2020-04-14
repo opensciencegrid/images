d88d22ebff27946036f4e589c03102f1622c859a
efajardo <efajardo@physics.ucsd.edu>
2020-04-14 12:47:36 -0700

    Fixing the right value for squid cache dir

--- opensciencegrid/frontier-squid/squid-customize.sh
+++ opensciencegrid/frontier-squid/squid-customize.sh
@@ -32,6 +32,6 @@ fi
 awk --file `dirname $0`/customhelps.awk --source "{
 setoption(\"acl NET_LOCAL src\", \"$SQUID_IPRANGE\")
 setoption(\"cache_mem\", \"$SQUID_CACHE_MEM\")
-setoptionparameter(\"cache_dir\", 3, \"$SQUID_CACHE_DISK\")
+setoptionparameter(\"cache_dir\", 2, \"$SQUID_CACHE_DISK\")
 print
 }"
