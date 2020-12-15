773df8b24a6c8c9769863e7e636cc3d53a73d88e
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2020-12-15 17:58:43 -0600

    use single quotes intead of double quotes for awk files

--- opensciencegrid/frontier-squid/customize.d/10-stdvars.awk
+++ opensciencegrid/frontier-squid/customize.d/10-stdvars.awk
@@ -1,4 +1,4 @@
-setoption(\"acl NET_LOCAL src\", \"$SQUID_IPRANGE\")
-setoption(\"cache_mem\", \"$SQUID_CACHE_MEM\")
-setoptionparameter(\"cache_dir\", 3, \"$SQUID_CACHE_DISK\")
-setoptionparameter(\"cache_dir\", 2, \"$SQUID_CACHE_DISK_LOCATION\")
+setoption("acl NET_LOCAL src", "'$SQUID_IPRANGE'")
+setoption("cache_mem", "'$SQUID_CACHE_MEM'")
+setoptionparameter("cache_dir", 3, "'$SQUID_CACHE_DISK'")
+setoptionparameter("cache_dir", 2, "'$SQUID_CACHE_DISK_LOCATION'")
--- opensciencegrid/frontier-squid/squid-customize.sh
+++ opensciencegrid/frontier-squid/squid-customize.sh
@@ -6,16 +6,18 @@
 # Note:
 #   It will not be overwritten by upgrades.
 # This can be further customized by adding pre-awk shell scripts in
-#  customize.d/[0-4]*.sh, awk function calls (which may include shell
-#  variable substitutions) in customize.d/*.awk, and/or post-awk shell
-#  scripts in customize.d/[5-9]*.sh.  The "print" awk function is in
-#  customize.d/90-print.awk so begin the filenames of awk function calls
-#  with a lower number than that if you want them to run before the print
-#  or a higher number if you want them after the print.  Generally only
-#  "insertline" function calls makes sense after the print, if you want
-#  to insert after the given line instead of before.
+#  customize.d/[0-4]*.sh, awk function calls in customize.d/*.awk,
+#  and/or post-awk shell scripts in customize.d/[5-9]*.sh.  Awk files
+#  may include shell variable substitutions, but the files are
+#  surrounded by single quotes so put a close single quote ahead of a
+#  variable and an open single quote after the variable.  The "print"
+#  awk function call is in customize.d/90-print.awk so begin the
+#  filenames of awk function calls with a lower number than that if you
+#  want them to run before the print or a higher number if you want them
+#  after the print.  Generally only "insertline" function calls make
+#  sense after the print, if you want to insert after the given line
+#  instead of before.
 # See customhelps.awk for information on predefined edit functions.
-# Quotes in the awk sources have to protected from bash.
 #
 
 cd `dirname $0`/customize.d
@@ -28,7 +30,7 @@ done
 
 
 awk --file ../customhelps.awk --source "{
-$(for f in *.awk; do eval "echo \"$(cat $f)\""; done)
+$(for f in *.awk; do eval "echo '$(cat $f)'"; done)
 }"
 
 for f in [5-9]*.sh; do
