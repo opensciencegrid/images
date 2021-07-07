92e880a185b5efe1803b899dcd0f43479d9b66d7
Brian Lin <brianhlin@gmail.com>
2021-07-07 15:01:27 -0500

    Add restore and ls subcommands

--- opensciencegrid/s3-backup/README.md
+++ opensciencegrid/s3-backup/README.md
@@ -1,12 +1,91 @@
 S3 Backup
 =========
 
-A container that archives files mounted to `/input`, encrypts the archive, and copies the encrypted archive to the
-specified S3 location.
-The encryption key must be mounted to `/encryption.key` and the following environment variables must be set:
+A container that can [encrypt and backup](#creating-backups) contents to an S3 bucket,
+[view](#viewing-backups) the contents of an S3 bucket,
+and [restore and decrypt backups](#restoring-backups) from an S3 bucket.
+
+The following environment variables are required for all sub-commands:
 
 - `S3_ACCESS_KEY`: S3 access key
-- `S3_BUCKET`: S3 bucket to copy encrypted files to
-- `S3_DEST_DIR`: directory within the S3 bucket to copy encrypted files to
+- `S3_BUCKET`: S3 bucket
 - `S3_SECRET_KEY`: S3 secret key
 - `S3_URL`: S3 endpoint URL. Must start with `https://`.
+
+Required for `backup` and `restore` operations:
+
+- `S3_DEST_DIR`: target directory relative to `S3_BUCKET` for creating/restoring backups and `ls` operations
+
+
+Creating Backups
+----------------
+
+In addition to the above environment variable requirements,
+creating backups requires data mounted to `/input` and an encryption key mounted to `/encryption.key`.
+Backup is the default operation.
+
+```
+docker run -it \
+           -v /path/to/encryption/key:/encryption.key \
+           -v /dir/containing/data/for/backup:/input \
+           --env-file path/to/environment/file \
+           s3-backup:release
+```
+
+Viewing Backups
+---------------
+
+Show all objects in `S3_BUCKET/S3_DESTDIR`:
+
+```
+docker run -it \
+           --env-file path/to/environment/file \
+           s3-backup:release \
+           ls
+```
+
+Show all objects in `S3_BUCKET/<ALTERNATE_DIR>`:
+
+```
+docker run -it \
+           --env-file path/to/environment/file \
+           s3-backup:release \
+           ls <ALTERNATE DIR>
+```
+
+To see the contents of the bucket root:
+
+```
+docker run -it \
+           --env-file path/to/environment/file \
+           s3-backup:release \
+           ls /
+```
+
+Restoring Backups
+-----------------
+
+In addition to the above environment variable requirements,
+creating backups requires an output directory mounted to `/output` and a decryption key mounted to `/encryption.key`.
+
+Copy and decrypt the latest backup:
+
+```
+docker run -it \
+           -v /path/to/encryption/key:/encryption.key \
+           -v /target/restore/dir:/output \
+           --env-file path/to/environment/file \
+           s3-backup:release
+           restore
+```
+
+Copy and decrypt the backup corresponding to a specific datetime, replacing `<YYYYMMDD-mmdd>` with the datetime:
+
+```
+docker run -it \
+           -v /path/to/encryption/key:/encryption.key \
+           -v /target/restore/dir:/output \
+           --env-file path/to/environment/file \
+           s3-backup:release
+           restore <YYYYMMDD-mmdd>
+```
--- opensciencegrid/s3-backup/s3-backup.sh
+++ opensciencegrid/s3-backup/s3-backup.sh
@@ -3,49 +3,142 @@
 set -e
 
 ENCRYPTION_KEY=/encryption.key
+INPUT_DIR=/input
+OUTPUT_DIR=/output
+S3_ALIAS=dest
 
-usage () {
-    cat <<EOF
-This tool requires an encryption key in /encryption-key
-and the following environment variables
-- S3_ACCESS_KEY
-- S3_BUCKET
-- S3_DEST_DIR
-- S3_SECRET_KEY
-- S3_URL (must start with https://)
-EOF
+fail () {
+    echo "$1"
     exit 1
 }
 
-# S3_* env vars are required, the url must be properly formatted, and
-# there must be an encryption key
-if [[ -z $S3_ACCESS_KEY$S3_BUCKET$S3_DEST_DIR$S3_SECRET_KEY$S3_URL ]] ||
-       [[ $S3_URL != https://* ]] ||
-       [[ ! -f $ENCRYPTION_KEY ]]; then
+usage () {
+    fail "Usage: $0 SUBCOMMAND [ARG]
+
+Subcommands:
+
+  backup                 Encrypts and backs up data to S3
+  ls [path]              Show contents of S3_DEST_DIR or 'path'
+                         ('/' shows the contents of the S3_BUCKET root)
+  restore [datetime]     Restore latest backup from S3 or backup corresponding to
+                         'datetime' (format YYYYMMDD-hhmm)
+
+Required environment variables:
+
+- S3_ACCESS_KEY
+- S3_BUCKET
+- S3_DEST_DIR (optional for the 'ls' subcommand)
+- S3_SECRET_KEY
+- S3_URL (must start with https://)"
+}
+
+
+mc_init_config () {
+    # Create an alias for the S3 endpoint/credentials
+    # S3_URL must start with "https://"
+    mc alias set dest "$S3_URL" "$S3_ACCESS_KEY" "$S3_SECRET_KEY"
+}
+
+
+mc_backup () {
+    [[ -d $INPUT_DIR ]] || fail "ERROR: Missing /input dir"
+    [[ -n $(ls $INPUT_DIR) ]] || fail "ERROR: no contents to backup in /input"
+    [[ -f $ENCRYPTION_KEY ]] || fail "ERROR: Missing encryption key, /encryption.key"
+
+    tmpdir=$(mktemp -d)
+    chmod 700 $tmpdir
+    BACKUP=$tmpdir/backup-$(date +%Y%m%d-%H%M).tar
+    tar -cvf $BACKUP -C $INPUT_DIR .
+
+    # Encrypt the tarball contents using a key mounted to
+    # $ENCRYPTION_KEY
+    gpg --batch \
+        --passphrase-file $ENCRYPTION_KEY \
+        --output "$BACKUP.enc" \
+        --symmetric \
+        --cipher-algo AES256 \
+        $BACKUP
+
+    mc cp \
+       $BACKUP.enc \
+       "$S3_ALIAS/$S3_BUCKET/$S3_DEST_DIR/"
+}
+
+
+mc_ls () {
+    # Treat / as the root of the bucket
+    if [[ $1 == / ]]; then
+        S3_DEST_DIR=""
+    # otherwise, show the contents of the first arg or an existing S3_DEST_DIR
+    elif [[ -n $1 ]]; then
+        S3_DEST_DIR="$1"
+    fi
+
+    [[ -n $S3_DEST_DIR ]] || usage
+
+    mc ls "$S3_ALIAS/$S3_BUCKET/$S3_DEST_DIR"
+}
+
+
+mc_restore () {
+    [[ -d $OUTPUT_DIR ]] || fail "ERROR: Missing /output dir"
+    [[ -f $ENCRYPTION_KEY ]] || fail "ERROR: Missing decryption key, /encryption.key"
+
+    if [[ $1 =~ [0-9]{8}+-[0-9]{4}+ ]]; then
+        BACKUP="backup-$1.tar.enc"
+    else
+        echo "Datetime not specified, finding latest backup..."
+        BACKUP=$(mc_ls | awk 'END{print $NF}')
+    fi
+
+    BACKUP_PATH=$S3_DEST_DIR/$BACKUP
+    # 'mc ls' appears to return 0 if the object doesn't exist
+    [[ -n $(mc_ls "$BACKUP_PATH") ]] || \
+        fail "ERROR: Failed to find backup at $BACKUP_PATH"
+
+    src_tmpfile=$(mktemp)
+    mc cp \
+       "$S3_ALIAS/$S3_BUCKET/$BACKUP_PATH" \
+       $src_tmpfile
+
+    dcrypt_tmpfile=$(mktemp)
+    gpg --batch \
+        --yes \
+        --passphrase-file $ENCRYPTION_KEY \
+        --output $dcrypt_tmpfile \
+        --decrypt \
+        $src_tmpfile
+
+    tar -xvf \
+        $dcrypt_tmpfile \
+        -C $OUTPUT_DIR
+}
+
+
+# Capture the subcommand
+case $# in
+    0) subcommand=backup ;;
+    # allow 2 args, ls/restore can take an additional arg
+    [12]) subcommand=$1 ;;
+    *) usage ;;
+esac
+
+# Bail if commonly required S3_* env vars aren't set
+if [[ -z $S3_ACCESS_KEY$S3_BUCKET$S3_SECRET_KEY$S3_URL ]] ||
+       [[ $S3_URL != https://* ]]; then
     usage
 fi
 
-# Create an alias for the S3 endpoint/credentials
-# S3_URL must start with "https://"
-mc alias set dest "$S3_URL" "$S3_ACCESS_KEY" "$S3_SECRET_KEY"
+mc_init_config
 
-
-tmpdir=$(mktemp -d)
-chmod 700 $tmpdir
-backup=$tmpdir/backup-$(date +%Y%m%d-%H%M).tar
-tar -cvf $backup /input/*
-
-# Encrypt the tarball contents using a key mounted to
-# $ENCRYPTION_KEY
-gpg --batch \
-    --passphrase-file $ENCRYPTION_KEY \
-    --output "$backup.enc" \
-    --symmetric \
-    --cipher-algo AES256 \
-    $backup
-
-# Copy 
-mc cp \
-   --preserve \
-   $backup \
-   "dest/$S3_BUCKET/$S3_DEST_DIR/"
+# Run subcommands
+case "$subcommand" in
+    backup)
+        mc_backup ;;
+    ls)
+        shift; mc_ls "$@" ;;
+    restore)
+        shift; mc_restore "$@" ;;
+    *)
+        usage ;;
+esac
