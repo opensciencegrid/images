d5465d0b27f7a914399e4a013f16d9faf0aff744
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-05-17 16:04:00 -0700

    Add scripts/check_master.sh

--- osg-htc/nrp-ospool-ep/scripts/entrypoint.sh
+++ osg-htc/nrp-ospool-ep/scripts/entrypoint.sh
@@ -3,5 +3,19 @@
 # Allow the derived images to run any additional runtime customizations
 for x in /etc/entrypoint/image-config.d/*.sh; do source "$x"; done
 
+# properly cleanup on signal
+trap 'echo signal received!; kill $(jobs -p); wait' SIGINT SIGTERM
+
 export HOME=/pilot
-su osg -p -c "/bin/entrypoint.osg.sh $@"
+su osg -p -c "/bin/entrypoint.osg.sh $@" &
+myproc=$!
+
+# protection in case it does not terminate oby itself when condor dies or restarts
+(/bin/check_master.sh 300 1; kill ${myproc}; echo "`date` Sending kill") &
+
+
+wait ${myproc}
+rc=$?
+echo "`date` entrypoint.osg.sh terminated with $rc"
+
+exit 0
