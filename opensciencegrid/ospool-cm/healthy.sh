8911a1e422be2035dd420a8c8e250d8efccb86a7
Mats Rynge <mats@rynge.net>
2023-03-21 10:46:24 -0700

    Update opensciencegrid/ospool-cm/healthy.sh
    Co-authored-by: Matyas Selmeci <matyas@cs.wisc.edu>

--- opensciencegrid/ospool-cm/healthy.sh
+++ opensciencegrid/ospool-cm/healthy.sh
@@ -6,7 +6,7 @@ if ! supervisorctl status >/dev/null 2>&1; then
 fi
 
 # we have seen the negotiator get into a funky auth state
-ERR_COUNT=$(tail -n 5000 /var/log/condor/NegotiatorLog 2>/dev/null | grep "ERROR: AUTHENTICATE" 2>/dev/null | wc -l)
+ERR_COUNT=$(tail -n 5000 /var/log/condor/NegotiatorLog 2>/dev/null | grep -c "ERROR: AUTHENTICATE" 2>/dev/null)
 if [ $ERR_COUNT -gt 50 ]; then
     echo "Excessive auth errors in the NegotiatorLog" >&2
     exit 3
