8ff5270c1cc08013f042c6a563deef8a9d0f05b1
Jeff Dost <jdost@ucsd.edu>
2023-05-18 10:35:10 -0700

    add missing chown to factory tarball bootstrapping

--- opensciencegrid/gwms-factory/etc/osg/image-init.d/16_prep_tarball_script.sh
+++ opensciencegrid/gwms-factory/etc/osg/image-init.d/16_prep_tarball_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this is just a temporary script until get_tarballs is merged in the upstream gwms codebase
+
 cp /etc/gwms-factory/get_tarballs.yaml.base /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.yaml
+chown gfactory: /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.yaml
 
 ln -s /opt/factools/hooks.reconfig.pre/get_tarballs.py /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.py
