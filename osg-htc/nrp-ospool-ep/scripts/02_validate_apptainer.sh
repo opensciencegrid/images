410761fd2ffa89e85eb76319886ddb4f9ca9ebf1
Igor Sfiligoi <isfiligoi@ucsd.edu>
2023-06-20 16:29:56 -0700

    Test also --nv in apptainer

--- osg-htc/nrp-ospool-ep/scripts/02_validate_apptainer.sh
+++ osg-htc/nrp-ospool-ep/scripts/02_validate_apptainer.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# check if we need to test nvidia
+nvf=
+ls -l /dev/nvidia*
+if [ $? -eq 0 ]; then
+  nvf=--nv
+fi
+
 # else do nothing, let Condor figure it out
 
 if [ -f "/usr/bin/apptainer" ]; then
   # only test for apptainer functionality if singularity is present
   # may not be in all pods
 
-  /usr/bin/apptainer exec --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest /usr/bin/dc -e "3 5 + p"
+  /usr/bin/apptainer exec $nvf --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest /usr/bin/dc -e "3 5 + p"
   rc=$?
 
   if [ $rc -ne 0 ]; then
