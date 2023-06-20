85d06f4da47080b2b4b69984de01038056a50d23
Igor Sfiligoi <isfiligoi@ucsd.edu>
2023-06-20 16:28:07 -0700

    Test also --nv in singularity

--- osg-htc/nrp-ospool-ep/scripts/02_validate_singularity.sh
+++ osg-htc/nrp-ospool-ep/scripts/02_validate_singularity.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# check if we need to test nvidia
+nvf=
+ls -l /dev/nvidia*
+if [ $? -eq 0 ]; then
+  nvf=--nv
+fi
+
 # else do nothing, let Condor figure it out
 
 if [ -f "/usr/bin/singularity" ]; then
   # only test for singularity functionality if singularity is present
   # may not be in all pods
 
-  /usr/bin/singularity exec --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest /usr/bin/dc -e "3 5 + p"
+  /usr/bin/singularity exec $nvf --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el8:latest /usr/bin/dc -e "3 5 + p"
   rc=$?
 
   if [ $rc -ne 0 ]; then
