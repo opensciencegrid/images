82aa7992d611a584194e64f0d24c8e95c4b3c729
Matyas Selmeci <matyas@cs.wisc.edu>
2023-08-01 12:22:27 -0500

    ospool-cm health check: ignore log messages from before container start
    Log files are preserved between restarts so the part of the health check
    that counts errors in the NegotiatorLog was constantly reporting a high
    count based on old log messages.  Only count log lines from after the
    container was started.
    
    Because timestamps for log messages are written in locale-specific time format,
    it's easier to parse them in Python.  I didn't rewrite the whole script in Python
    because correctly writing `tail` in Python is hard.

--- opensciencegrid/ospool-cm/healthy.sh
+++ opensciencegrid/ospool-cm/healthy.sh
@@ -5,11 +5,47 @@ if ! supervisorctl status >/dev/null 2>&1; then
     exit 2
 fi
 
+container_start_time=$(stat -c %Z /proc/1)  # ctime, epoch time
+
+count_errors () {
+    local file="$1"
+    local lines="$2"
+    local starttime="$3"
+
+    tail -n $lines $file | python3 -c '
+import sys
+import time
+
+# container start time is in epoch format
+starttime = time.gmtime(int(sys.argv[1]))
+
+error_count = 0
+for line in sys.stdin:
+    # split the line into (year, time, everything else)
+    split_line = line.split(" ", 2)
+    try:
+        # parse the timestamp -- should look like "08/01/23 11:47:36"
+        linetime = time.strptime("%s %s" % split_line[0:2], "%m/%d/%y %H:%M:%S")
+    except (TypeError, ValueError):
+        # could not parse it; skip this line
+        continue
+    if linetime < starttime:
+        # this happened before container startup, skip it
+        continue
+    if "ERROR: AUTHENTICATE" in line:
+        error_count += 1
+
+print(error_count)
+' $starttime
+}
+
 # we have seen the negotiator get into a funky auth state
-ERR_COUNT=$(tail -n 5000 /var/log/condor/NegotiatorLog 2>/dev/null | grep -c "ERROR: AUTHENTICATE" 2>/dev/null)
-if [ $ERR_COUNT -gt 50 ]; then
-    echo "Excessive auth errors in the NegotiatorLog" >&2
-    exit 3
+if [ -r /var/log/condor/NegotiatorLog ]; then
+    ERR_COUNT=$(count_errors /var/log/condor/NegotiatorLog 5000 $container_start_time)
+    if [ $ERR_COUNT -gt 50 ]; then
+        echo "Excessive auth errors in the NegotiatorLog" >&2
+        exit 3
+    fi
 fi
 
 exit 0
