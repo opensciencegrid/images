356ef0f2c697bf7da5ed09850eabb621dded573f
Matyas Selmeci <matyas@cs.wisc.edu>
2023-08-01 14:17:16 -0500

    Simplify code

--- opensciencegrid/ospool-cm/healthy.sh
+++ opensciencegrid/ospool-cm/healthy.sh
@@ -7,17 +7,9 @@ fi
 
 container_start_time=$(stat -c %Z /proc/1)  # ctime, epoch time
 
-count_errors () {
-    local file="$1"
-    local lines="$2"
-    local starttime="$3"
-
-    tail -n "$lines" "$file" | count_errors.py "$starttime"
-}
-
 # we have seen the negotiator get into a funky auth state
 if [ -r /var/log/condor/NegotiatorLog ]; then
-    ERR_COUNT=$(count_errors /var/log/condor/NegotiatorLog 5000 "$container_start_time")
+    ERR_COUNT=$(tail -n 5000 /var/log/condor/NegotiatorLog | count_errors.py "$container_start_time")
     if [ "$ERR_COUNT" -gt 50 ]; then
         echo "Excessive auth errors in the NegotiatorLog" >&2
         exit 3
