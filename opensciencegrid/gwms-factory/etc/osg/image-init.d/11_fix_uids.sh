80df1e069900dce02c0434feea43305d04033a85
jdost321 <jdost@ucsd.edu>
2023-10-05 13:37:09 -0700

    Update opensciencegrid/gwms-factory/etc/osg/image-init.d/11_fix_uids.sh
    Co-authored-by: jthiltges <john@thiltges.com>

--- opensciencegrid/gwms-factory/etc/osg/image-init.d/11_fix_uids.sh
+++ opensciencegrid/gwms-factory/etc/osg/image-init.d/11_fix_uids.sh
@@ -6,9 +6,11 @@ if [ $(stat -c %U:%G /var/lib/condor/spool) != "condor:condor" ] ; then
   chown -R condor:condor /var/lib/condor/*
 fi
 
-if [ $(stat -c %U:%G /var/lib/gwms-factory/web-area/stage) != "gfactory:gfactory" ] ; then
-  chown -R gfactory:gfactory /var/lib/gwms-factory/*
-fi
+for entry in /var/lib/gwms-factory/* ; do
+  if [ $(stat -c %U:%G "$entry") != "gfactory:gfactory" ] ; then
+    chown -R gfactory:gfactory "$entry"
+  fi
+done
 
 if [ $(stat -c %U:%G /var/log/gwms-factory/server) != "gfactory:gfactory" ] ; then
   chown -R gfactory:gfactory /var/log/gwms-factory/*
