43821a88ff61aff787d480a80975a29cf58a9750
Jeff Dost <jdost@ucsd.edu>
2024-08-02 12:17:51 -0700

    symlink the get_tarballs from /usr/bin since it has the right permissions

--- opensciencegrid/gwms-factory/etc/osg/image-init.d/16_prep_tarball_script.sh
+++ opensciencegrid/gwms-factory/etc/osg/image-init.d/16_prep_tarball_script.sh
@@ -6,4 +6,4 @@ cp /etc/gwms-factory/get_tarballs.yaml.base /etc/gwms-factory/hooks.reconfig.pre
 chown gfactory: /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.yaml
 
 # enable tarball script as a pre-reconfig hook
-ln -s /usr/lib/python3.9/site-packages/glideinwms/factory/tools/get_tarballs.py /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.py
+ln -s /usr/bin/get_tarballs /etc/gwms-factory/hooks.reconfig.pre/get_tarballs
